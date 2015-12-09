8f5efcdc09fb0e5b55116c007df87171a37c00d4
holyorb2 <holyorb2@gmail.com>
2015-12-09 11:15:55 +0200

    Refactor code

--- hosts.py
+++ hosts.py
@@ -1,27 +1,49 @@
 #!/usr/bin/env python3
 
-mark = { 'start': '### auto_hosts-start', 'end' : '### auto_hosts-end' }
-pos = { 'start': 0, 'end': 0 }
+def resave_hosts(file_path, insert = None, mark = None) :
+  if mark is None :
+    mark = { 'start': '### auto_hosts-start', 'end' : '### auto_hosts-end' }
 
-insert = [{ 'ip' : '10.10.10.10', 'test.cm1'}, { 'ip' : '10.10.10.11', 'test1.cm2 test2.cm2'}]
+  a_share_hosts = []
+  a_my_hosts = []
+  f_curr = 'share'
 
-a_file = []
-i = 0
-for s_line in open('hosts', 'r'):
-  if len(s_line) > 1 :
-    if pos['start'] == 0 and s_line == mark['start'] :
-      pos['start'] = i
-    if pos['end'] == 0 and s_line == mark['end'] :
-      pos['end'] = i
-    print ('{0} - {1}'.format(i, s_line[:-1]))
-  i += 1
-  a_file.append(s_line)
+  # File loads.
+  for s_line in open(file_path, 'r') :
+    s_line = s_line.strip()
+    if len(s_line) > 1 :
+      if s_line == mark['start'] :
+        f_curr = 'my'
+        continue
+      if s_line == mark['end'] :
+        f_curr = 'share'
+        continue
 
-print ('Start = {0}; End = {1}'.format(pos['start'], pos['end']))
-print (a_file)
+    if f_curr == 'share' :
+      a_share_hosts.append(s_line)
+    else:
+      if len(s_line) > 1:
+        a_my_hosts.append(s_line)
+
+  # File saves.
+  f_hosts = open(file_path, 'w')
+  i_br = 0
+  for s_line in a_share_hosts :
+    i_br = 0 if len(s_line) > 1 else i_br + 1
+    if i_br < 3 :
+      f_hosts.write(s_line + '\n')
+
+  if insert is not None :
+    if i_br < 3 :
+      f_hosts.write('\n')
+    f_hosts.write(mark['start'] + '\n')
+    for s_line in insert :
+      f_hosts.write(s_line['ip'] + '  ' + s_line['sites'] + '\n')
+    f_hosts.write(mark['end'] + '\n')
+
+  f_hosts.close()
 
 
+insert = [{ 'ip' : '10.10.10.10', 'sites': 'test.cm1'}, { 'ip' : '10.10.10.11', 'sites': 'test1.cm2 test2.cm2'}]
 
-
-
-# 'Last:\n{0} ({5})\n{1}\n\nAll answer - {2}\nError - {3}\nRight - {4}'.format(last, status)
+resave_hosts('hosts', insert)
