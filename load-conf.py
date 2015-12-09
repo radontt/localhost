ed80abe9430381032ae14132779ac266feb38c6a
holyorb <holyorb2@gmail.com>
2015-12-09 22:01:37 +0200

    Add test configuration

--- load-conf.py
+++ load-conf.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
+import yaml
 
-def load_conf(file_path):
-  #
+def load_conf(file_path) :
+  with open('tmp/main-sites.conf', 'r') as stream :
+    conf = yaml.load(stream)
 
+  
 
 ##########################################
 load_conf('tmp/main-sites.conf')
