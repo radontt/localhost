a0613926639e791330a2b15a6cc1ce8b6d1a89c3
holyorb2 <holyorb2@gmail.com>
2015-12-10 15:43:25 +0200

    Load config and parse sites in a status "turn on"

--- load-conf.py
+++ load-conf.py
@@ -4,8 +4,24 @@ import yaml
 def load_conf(file_path) :
   with open('tmp/main-sites.conf', 'r') as stream :
     conf = yaml.load(stream)
+  return conf
 
-  
+def get_conf_hosts(conf) :
+  a_hosts = []
+  for ip in conf :
+    if 'status' not in conf[ip] or conf[ip]['status'] == 1 :
+      a_sites = conf[ip]['sites']
+      site_list = []
+      for item in a_sites :
+        if 'status' not in a_sites[item] or a_sites[item]['status'] == 1 :
+          site_list.append(item)
+      if len(site_list) :
+        a_hosts.append({'ip': ip, 'sites': site_list})
+  return a_hosts
+
+
 
 ##########################################
-load_conf('tmp/main-sites.conf')
+conf = load_conf('tmp/main-sites.conf')
+hosts = get_conf_hosts(conf)
+print (hosts)
