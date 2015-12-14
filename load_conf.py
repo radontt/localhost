8b46e770d599da24111b6d43f79c06e59cd27c44
holyorb2 <holyorb2@gmail.com>
2015-12-14 20:27:16 +0200

    Change functions to class object

--- load_conf.py
+++ load_conf.py
@@ -1,20 +1,27 @@
 #!/usr/bin/env python3
 import yaml
 
-def load_conf(file_path) :
-  with open('tmp/main-sites.conf', 'r') as stream :
-    conf = yaml.load(stream)
-  return conf
+class Config:
+  conf = []
 
-def get_conf_hosts(conf) :
-  a_hosts = []
-  for ip in conf :
-    if 'status' not in conf[ip] or conf[ip]['status'] == 1 :
-      a_sites = conf[ip]['sites']
-      site_list = []
-      for item in a_sites :
-        if 'status' not in a_sites[item] or a_sites[item]['status'] == 1 :
-          site_list.append(item)
-      if len(site_list) :
-        a_hosts.append({'ip': ip, 'sites': site_list})
-  return a_hosts
+  def __init__(self, file_path = None):
+    load_conf(file_path)
+
+  # Configuration file load.
+  def load_conf(self, file_path):
+    with open(file_path, 'r') as f_conf:
+      self.conf = yaml.load(f_conf)
+
+  # Get hosts from configuration.
+  def get_conf_hosts(self):
+    a_hosts = []
+    for ip in self.conf:
+      if 'status' not in self.conf[ip] or self.conf[ip]['status'] == 1 :
+        a_sites = self.conf[ip]['sites']
+        site_list = []
+        for item in a_sites:
+          if 'status' not in a_sites[item] or a_sites[item]['status'] == 1 :
+            site_list.append(item)
+        if len(site_list):
+          a_hosts.append({'ip': ip, 'sites': site_list})
+    return a_hosts
