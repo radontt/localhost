c9cad8b8185cddd95b1f65265ecd430f9979a44f
holyorb2 <holyorb2@gmail.com>
2015-12-18 14:42:47 +0200

    Filled tabs

--- docker.py
+++ docker.py
@@ -11,17 +11,29 @@ class MyWindow(Gtk.Window):
     self.notebook = Gtk.Notebook()
     self.add(self.notebook)
 
-    self.p_list = Gtk.Box()
+    self.p_list = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
     self.p_list.set_border_width(10)
-    self.p_list.add(Gtk.Label('List of dockers'))
+    p = os.popen('docker ps -a')
+    line = True
+    s_ps = ''
+    while line:
+      line = p.readline()
+      s_ps = s_ps + line
+    self.p_list.pack_start(Gtk.Label(s_ps), False, False, 0)
     self.notebook.append_page(self.p_list, Gtk.Label('Status'))
 
-    self.p_images = Gtk.Box()
+    self.p_images = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
     self.p_images.set_border_width(10)
-    self.p_images.add(Gtk.Label('List of images'))
+    p = os.popen('docker images')
+    line = True
+    images = ''
+    while line:
+      line = p.readline()
+      images = images + line
+    self.p_images.pack_start(Gtk.Label(images), False, True, 0)
     self.notebook.append_page(self.p_images, Gtk.Label('Images'))
 
-    self.p_about = Gtk.Box()
+    self.p_about = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
     self.p_about.set_border_width(10)
     p = os.popen('docker version')
     line = True
@@ -29,7 +41,7 @@ class MyWindow(Gtk.Window):
     while line:
       line = p.readline()
       version = version + line
-    self.p_about.add(Gtk.Label(version))
+    self.p_about.pack_start(Gtk.Label(version), False, False, 0)
     self.notebook.append_page(self.p_about, Gtk.Label('About'))
 
 win = MyWindow()
